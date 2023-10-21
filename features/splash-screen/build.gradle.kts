55d2815e5eb713e65563ccd524097d95fe3b8baf
isfaaghyth <isfaaghyth@gmail.com>
2023-10-21 08:18:57 +0700

    feat: create shared common library gradle

--- features/splash-screen/build.gradle.kts
+++ features/splash-screen/build.gradle.kts
@@ -1,3 +1,5 @@
+apply(from = "${rootProject.projectDir}/common.library.gradle")
+
 plugins {
     id("com.android.library")
     id("org.jetbrains.kotlin.android")
@@ -5,41 +7,6 @@ plugins {
 
 android {
     namespace = "com.isfa.splash"
-    compileSdk = 33
-
-    defaultConfig {
-        minSdk = 24
-
-        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
-        consumerProguardFiles("consumer-rules.pro")
-    }
-
-    buildTypes {
-        release {
-            isMinifyEnabled = false
-            proguardFiles(
-                getDefaultProguardFile("proguard-android-optimize.txt"),
-                "proguard-rules.pro"
-            )
-        }
-    }
-    compileOptions {
-        sourceCompatibility = JavaVersion.VERSION_1_8
-        targetCompatibility = JavaVersion.VERSION_1_8
-    }
-    kotlinOptions {
-        jvmTarget = "1.8"
-    }
 }
 
-dependencies {
-
-    implementation(libs.core.ktx)
-    implementation(libs.appcompat)
-    implementation(libs.constraintlayout)
-    implementation(libs.material)
-
-    testImplementation(libs.junit)
-    androidTestImplementation(libs.androidx.junit)
-    androidTestImplementation(libs.androidx.espresso)
-}
+dependencies {}
